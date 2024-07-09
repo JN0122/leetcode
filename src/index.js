294c06e49338d16132472335d20a6146e52b8aaa
Jakub Niewelt <jakubniewelt@icloud.com>
2024-07-09 11:07:20 +0200

    1701. Average Waiting Time

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-function findTheWinner(n, k) {
-    var friendsInCircle = new Array();
-    for (var i = 1; i <= n; i++)
-        friendsInCircle.push(i);
-    var index = 0;
-    while (friendsInCircle.length > 1) {
-        index = (k - 1 + index) % friendsInCircle.length;
-        friendsInCircle.splice(index, 1);
+function averageWaitingTime(customers) {
+    var prepTime = [];
+    var chefFreeAt = 0;
+    var additionalWaitTime;
+    for (var i = 0; i < customers.length; i++) {
+        additionalWaitTime = 0;
+        if (chefFreeAt <= customers[i][0]) {
+            chefFreeAt = customers[i][0] + customers[i][1];
+        }
+        else {
+            additionalWaitTime = chefFreeAt - customers[i][0];
+            chefFreeAt += customers[i][1];
+        }
+        prepTime.push(customers[i][1] + additionalWaitTime);
     }
-    return friendsInCircle[0];
+    return (prepTime.reduce(function (totalTime, waitTime) { return waitTime + totalTime; }, 0) /
+        customers.length);
 }
-console.log(findTheWinner(6, 5));
-console.log(findTheWinner(5, 2));
+console.log(averageWaitingTime([
+    [1, 2],
+    [2, 5],
+    [4, 3],
+])); // 5
+console.log(averageWaitingTime([
+    [5, 2],
+    [5, 4],
+    [10, 3],
+    [20, 1],
+])); // 3.25
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
-function findTheWinner(n: number, k: number): number {
-  const friendsInCircle = new Array<number>();
-  for (let i = 1; i <= n; i++) friendsInCircle.push(i);
-
-  let index = 0;
-  while (friendsInCircle.length > 1) {
-    index = (k - 1 + index) % friendsInCircle.length;
-    friendsInCircle.splice(index, 1);
+function averageWaitingTime(customers: number[][]): number {
+  const prepTime: number[] = [];
+  let chefFreeAt = 0;
+  let additionalWaitTime: number;
+  for (let i = 0; i < customers.length; i++) {
+    additionalWaitTime = 0;
+    if (chefFreeAt <= customers[i][0]) {
+      chefFreeAt = customers[i][0] + customers[i][1];
+    } else {
+      additionalWaitTime = chefFreeAt - customers[i][0];
+      chefFreeAt += customers[i][1];
+    }
+    prepTime.push(customers[i][1] + additionalWaitTime);
   }
-  return friendsInCircle[0];
+  return (
+    prepTime.reduce((totalTime, waitTime) => waitTime + totalTime, 0) /
+    customers.length
+  );
 }
-console.log(findTheWinner(6, 5));
-console.log(findTheWinner(5, 2));
+console.log(
+  averageWaitingTime([
+    [1, 2],
+    [2, 5],
+    [4, 3],
+  ])
+); // 5
+console.log(
+  averageWaitingTime([
+    [5, 2],
+    [5, 4],
+    [10, 3],
+    [20, 1],
+  ])
+); // 3.25

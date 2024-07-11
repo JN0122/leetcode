844cfddecd81ee216119468146f1f60e2d2521b9
Jakub Niewelt <jakubniewelt@icloud.com>
2024-07-11 16:56:43 +0200

    fix: dummy index.ts

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-function minOperations(logs) {
-    var dirLevel = 0;
-    for (var _i = 0, logs_1 = logs; _i < logs_1.length; _i++) {
-        var log = logs_1[_i];
-        switch (log) {
-            case "../":
-                if (dirLevel > 0)
-                    dirLevel--;
-                break;
-            case "./":
-                break;
-            default:
-                dirLevel++;
-        }
-    }
-    return dirLevel;
+function test(s) {
+    return s;
 }
-console.log(minOperations(["d1/", "d2/", "../", "d21/", "./"])); // 2
-console.log(minOperations(["d1/", "d2/", "./", "d3/", "../", "d31/"])); // 3
-console.log(minOperations(["d1/", "../", "../", "../"])); // 0
+console.log(test("test")); // test
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
-function minOperations(logs: string[]): number {
-  let dirLevel = 0;
-  for (const log of logs) {
-    switch (log) {
-      case "../":
-        if (dirLevel > 0) dirLevel--;
-        break;
-      case "./":
-        break;
-      default:
-        dirLevel++;
-    }
-  }
-  return dirLevel;
+function test(s: string): string {
+  return s;
 }
-console.log(minOperations(["d1/", "d2/", "../", "d21/", "./"])); // 2
-console.log(minOperations(["d1/", "d2/", "./", "d3/", "../", "d31/"])); // 3
-console.log(minOperations(["d1/", "../", "../", "../"])); // 0
+console.log(test("test")); // test
